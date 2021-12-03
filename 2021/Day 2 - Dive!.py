8b37e19206fd4fa70b014d41b652e9e66a9c47d9
Ahmet Emre Usta <a.emreusta@hotmail.com>
2021-12-03 12:31:30 +0300

    Day 2 done!

---
+++ 2021/Day 2 - Dive!.py
@@ -0,0 +1,37 @@
+#Reading file
+with open(".\Inputs\day2.txt") as f:
+    list = [line.strip().split(' ') for line in f.readlines()] 
+f.close()
+
+#Part 1 Solution
+depth, horizontal = 0, 0
+
+for command, value in list:
+
+    if command == 'forward':
+        horizontal += int(value)
+        
+    elif command == 'down':
+        depth += int(value)
+        
+    elif command == 'up':
+        depth -= int(value)
+        
+print(horizontal*depth)
+
+#Part 2 Solution
+depth, horizontal, aim = 0, 0, 0
+
+for command, value in list:
+
+    if command == 'forward':
+        horizontal += int(value)
+        depth += int(value) * aim
+        
+    elif command == 'down':
+        aim += int(value)
+        
+    elif command == 'up':
+        aim -= int(value)
+        
+print(horizontal*depth)
