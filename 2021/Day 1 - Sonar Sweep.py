6a5154f357f73bda238303775730aa3e0b585e9c
Ahmet Emre Usta <a.emreusta@hotmail.com>
2021-12-03 11:54:25 +0300

    Hello Internet
    Advent of Code

---
+++ 2021/Day 1 - Sonar Sweep.py
@@ -0,0 +1,23 @@
+#Reading file
+with open(".\Inputs\day1.txt") as f:
+    list = [int(line.strip()) for line in f.readlines()] 
+f.close()
+
+#Part 1 Solution
+count = 0
+
+for i in range(1,len(list)):
+    if list[i] > list[i-1]:
+        count += 1
+        
+print(count)
+
+#Part 2 Solution
+count = 0
+
+for i in range(len(list)):
+    
+    if sum(list[i : i+3]) < sum(list[(i+1) : (i+1) + 3]):
+        count += 1
+        
+print(count)
