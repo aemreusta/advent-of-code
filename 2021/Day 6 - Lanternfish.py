6b17d716abefebdb0bfa57293a8d331075a71a62
Ahmet Emre Usta <a.emreusta@hotmail.com>
2021-12-07 16:05:53 +0300

    Day 6 Done!

---
+++ 2021/Day 6 - Lanternfish.py
@@ -0,0 +1,34 @@
+#Reading file
+with open(".\Inputs\day6.txt") as f:
+    data = [line.strip() for line in f.readlines()]
+f.close()
+
+#Part 1 Solution
+init_fish = list(map(int, data[0].split(",")))
+fish_list = [0]*9
+
+for fish in init_fish:
+    fish_list[fish] += 1
+
+for _ in range(80):
+    births = fish_list.pop(0)
+    fish_list.append(births)
+    fish_list[6] += births
+    
+print(sum(fish_list))
+#363101
+
+#Part 2 Solution
+init_fish = list(map(int, data[0].split(",")))
+fish_list = [0]*9
+
+for fish in init_fish:
+    fish_list[fish] += 1
+
+for _ in range(256):
+    births = fish_list.pop(0)
+    fish_list.append(births)
+    fish_list[6] += births
+    
+print(sum(fish_list))
+#1644286074024
